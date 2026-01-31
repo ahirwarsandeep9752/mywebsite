fb77db4ecc551ab18aebc8a041bd17eabde193e5
Sandeep Ahirwar <ahirwarsandeep9752@gmail.com>
2026-01-31 14:07:28 +0530

    Add hook page route

--- app.py
+++ app.py
@@ -66,3 +66,6 @@ def register():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render dynamic port
     app.run(host="0.0.0.0", port=port)
+@app.route("/hook")
+def hook():
+    return render_template("hook.html")
